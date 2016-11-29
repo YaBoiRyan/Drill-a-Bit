d117d8143c4da6017d39c6e26efb6fd537d85985
DarthSaffa <rowen.koortzen@gmail.com>
2016-11-30 01:10:41 +1000

    Finally did some work, not a lot tho

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Transform myTransform;
+	float topSpeed = 0.5f;
+	public float currentSpeed;
+	int rotateSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+
+		myTransform = transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Setting the updown speeds
+		if (Input.GetAxisRaw ("Vertical") > 0) {
+			currentSpeed = topSpeed;
+		} else {
+			currentSpeed = topSpeed/2;
+		}
+
+		//Movement
+		myTransform.Translate(Vector2.up * currentSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+		if(Input.GetAxis("Vertical") != 0){
+		myTransform.Rotate (Vector3.back * rotateSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+		}
+	}
+}
