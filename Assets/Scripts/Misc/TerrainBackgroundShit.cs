b5a2b65b0b359ad256a63578916217b638feffee
DarthSaffa <rowen123>
2016-12-01 18:30:33 +1000

    Broke some things

--- Assets/Scripts/Misc/TerrainBackgroundShit.cs
+++ Assets/Scripts/Misc/TerrainBackgroundShit.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class TerrainBackgroundShit : MonoBehaviour {
 
 	SpriteRenderer mySpriteRenderer;
+	Collider2D myCollider;
 	public Sprite poopDirt;
 
 	// Use this for initialization
 	void Start () {
 
 		mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		myCollider = gameObject.GetComponent<Collider2D>();
 
 	
 	}
@@ -24,6 +26,8 @@ public class TerrainBackgroundShit : MonoBehaviour {
 		if(other.CompareTag("Drill"))
 		{
 			mySpriteRenderer.sprite = poopDirt;
+			myCollider.enabled = false;
+
 		}
 
 	}
