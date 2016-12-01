44c78921d3260199568a9c2f1ea6653aa7c6a4cd
DarthSaffa <rowen123>
2016-12-01 17:38:00 +1000

    I am physically disgusted by how inefficient this is

---
+++ Assets/Scripts/Misc/CameraScroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScroll : MonoBehaviour {
+
+	GameObject driller;
+
+	// Use this for initialization
+	void Start () {
+
+		driller = GameObject.Find("Drillbert");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Misc/TerrainBackgroundShit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainBackgroundShit : MonoBehaviour {
+
+	SpriteRenderer mySpriteRenderer;
+	public Sprite poopDirt;
+
+	// Use this for initialization
+	void Start () {
+
+		mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.CompareTag("Drill"))
+		{
+			mySpriteRenderer.sprite = poopDirt;
+		}
+
+	}
+}
